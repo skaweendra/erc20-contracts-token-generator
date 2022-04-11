8baae7f840e260e8d09995600badd92148e64698
salitha1 <salitha@gl.block-stars.com>
2022-04-11 12:43:29 +0530

    adding Standard ERC20 Contract

--- StandardERC20/StandardERC20.sol
+++ StandardERC20/StandardERC20.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "./ERC20.sol";
 
-contract SimpleERC20 is ERC20 {
+contract StandardERC20 is ERC20 {
     constructor(string memory name, string memory symbol, uint8 decimal, uint256 totalSupply, uint256 initialSupply) ERC20(name, symbol, decimal, totalSupply, initialSupply) {
         _mint(msg.sender, 1000 * 10 ** decimals());
     }
